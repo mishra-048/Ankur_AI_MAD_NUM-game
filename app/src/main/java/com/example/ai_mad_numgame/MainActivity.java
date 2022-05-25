d54d2a8f4a174711f2b1f593c9b6e2ca594f3acf
Ankur <201b048@juetguna.in>
2022-05-26 02:55:05 +0530

    Completed the project code

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -166,13 +166,13 @@ public class MainActivity extends AppCompatActivity {
     public String getInterpretation(int [][]dataFrame,double slope){
         //provide interpretation based on your slope analysis
         // Your code here
-        String s="good keep it up";
+        String s="Good keep it up";
         if(slope==0 && dataFrame[0][1]==3)
-            return "your not attentive";
+            return "Your not attentive";
         else if(slope>0 && slope<5.0)
-            return "slow and steady";
+            return "Slow and steady";
         else if(slope>5.0)
-            return "your getting better with time";
+            return "Your getting better with time";
 
 //        return "Your Interpretation";
         return s;
