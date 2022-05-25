113bd124b40ab973c4ccc685445fb68a29d4f15f
Ankur <201b048@juetguna.in>
2022-05-26 02:29:16 +0530

    The code for calculating the sum of the score

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -141,7 +141,13 @@ public class MainActivity extends AppCompatActivity {
 
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
-
+        int sum=0;
+        for(int i=0;i<3;i++)
+        {
+            sum+=score[i];
+        }
+        // your code here
+        return sum;
     }
 
     public int[][] dataPrep() {
